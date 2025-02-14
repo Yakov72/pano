b0756ab191615dccf1951300ab5761c154bb09fb
Yakov72 <chyl@mail.ru>
2025-02-14 04:35:28 +0300

    ТИЦ загрузка

---
+++ tic/bitrix/cache/js/s1/f5-pomorland/page_c02354cc6b4cf702901ce28bd06942c4/page_c02354cc6b4cf702901ce28bd06942c4_v167ae.js
@@ -0,0 +1,15 @@
+
+; /* Start:"a:4:{s:4:"full";s:77:"/bitrix/components/f5studio/shares/templates/.default/script.js?1484132035136";s:6:"source";s:63:"/bitrix/components/f5studio/shares/templates/.default/script.js";s:3:"min";s:0:"";s:3:"map";s:0:"";}"*/
+function shareClick( el ){
+    window.open($(el).attr('href'),'_blank','width=440,height=420,location=0,status=no');
+    return false;
+}
+/* End */
+;
+; /* Start:"a:4:{s:4:"full";s:101:"/local/templates/f5-pomorland/components/bitrix/system.pagenavigation/show-more/script.js?14830113802";s:6:"source";s:89:"/local/templates/f5-pomorland/components/bitrix/system.pagenavigation/show-more/script.js";s:3:"min";s:0:"";s:3:"map";s:0:"";}"*/
+
+
+
+/* End */
+;; /* /bitrix/components/f5studio/shares/templates/.default/script.js?1484132035136*/
+; /* /local/templates/f5-pomorland/components/bitrix/system.pagenavigation/show-more/script.js?14830113802*/
